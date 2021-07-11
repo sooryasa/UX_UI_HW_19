0b13dab7770cb39012dc3038ed365fe32ff74566
sooryasa <77978755+sooryasa@users.noreply.github.com>
2021-07-11 14:10:21 -0400

    JS update

---
+++ js/index.js
@@ -0,0 +1,7 @@
+console.log ("Your index.js file is loaded correctly!")
+
+$("li").hover(function(){
+    $(this).css("background-color", "#6a8d92");
+    }, function(){
+    $(this).css("background-color", "white");
+  });
