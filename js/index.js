6e4a551c2e21e3e9ca1644050c77fc138a775252
sooryasa <77978755+sooryasa@users.noreply.github.com>
2021-07-11 14:16:40 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -5,3 +5,9 @@ $("li").hover(function(){
     }, function(){
     $(this).css("background-color", "white");
   });
+
+  $("button").hover(function(){
+    $(this).css("background-color", "#A8036C");
+    }, function () {
+    $(this).css("background-color", "#373738");
+  });
